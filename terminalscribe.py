a52e9511eb02e003419e0fd8f8d0d34138c1e8fa
Joseph SENGER <TT5612@engie.com>
2022-09-09 14:08:38 -0700

    Added terminalscribe.py and canvas.py

---
+++ terminalscribe.py
@@ -0,0 +1,37 @@
+import time
+from termcolor import colored
+
+class TerminalScribe:
+    def __init__(self, canvas):
+        self.canvas = canvas
+        self.trail = '.'
+        self.mark = '*'
+        self.framerate = 0.2
+        self.pos = [0, 0]
+
+    def up(self):
+        pos = [self.pos[0], self.pos[1]-1]
+        if not self.canvas.hitsWall(pos):
+            self.draw(pos)
+
+    def down(self):
+        pos = [self.pos[0], self.pos[1]+1]
+        if not self.canvas.hitsWall(pos):
+            self.draw(pos)
+
+    def right(self):
+        pos = [self.pos[0]+1, self.pos[1]]
+        if not self.canvas.hitsWall(pos):
+            self.draw(pos)
+
+    def left(self):
+        pos = [self.pos[0]-1, self.pos[1]]
+        if not self.canvas.hitsWall(pos):
+            self.draw(pos)
+
+    def draw(self, pos):
+        self.canvas.setPos(self.pos, self.trail)
+        self.pos = pos
+        self.canvas.setPos(self.pos, colored(self.mark, 'red'))
+        self.canvas.print()
+        time.sleep(self.framerate)
